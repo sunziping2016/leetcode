bfab3bd70f7d7debd0bd4ac683dcd0ae94c40bb3
Sun Ziping <me@szp.io>
2022-02-17 04:44:39 +0800

    add solution for valid-parentheses

--- 0020.valid-parentheses.rust/src/lib.rs
+++ 0020.valid-parentheses.rust/src/lib.rs
@@ -1,8 +1,35 @@
+/// https://leetcode-cn.com/problems/valid-parentheses/
+pub struct Solution;
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        for ch in s.as_bytes().iter().copied() {
+            match ch {
+                b'(' => stack.push(b')'),
+                b'[' => stack.push(b']'),
+                b'{' => stack.push(b'}'),
+                x => {
+                    if !stack.pop().map(|y| x == y).unwrap_or(false) {
+                        return false;
+                    }
+                }
+            }
+        }
+        stack.is_empty()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_example() {
+        assert!(Solution::is_valid("()".into()));
+        assert!(Solution::is_valid("()[]{}".into()));
+        assert!(!Solution::is_valid("(]".into()));
+        assert!(!Solution::is_valid("([)]".into()));
+        assert!(Solution::is_valid("{[]}".into()));
     }
 }
